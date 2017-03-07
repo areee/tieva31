144bad2688a14183a0412e4f4e26075456f5d10f
areee <areee@users.noreply.github.com>
2017-03-07 23:35:43 +0200

    test app updated according to 2nd lecture

--- src/App.js
+++ src/App.js
@@ -1,19 +1,80 @@
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper'
+import TextField from 'material-ui/TextField'
+import Checkbox from 'material-ui/Checkbox'
+import RaisedButton from 'material-ui/RaisedButton'
+
+const RATE = 1.01;
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      euros: 0,
+      dollars: 0,
+      doRound: false,
+    }
+  }
+
+updateEuros = (newValue) =>{
+  const eurs = parseFloat(newValue);
+  let dlrs = eurs*RATE;
+  if(this.state.doRound)
+    dlrs=Math.round(dlrs);
+  this.setState({
+    euros: eurs,
+    dollars: dlrs,
+    doRound: this.state.doRound,
+  });
+}
+
+updateDollars = (event, newValue)=>{
+  const dlrs = parseFloat(newValue);
+  const eurs = dlrs/RATE;
+  this.setState({
+    euros: eurs,
+    dollars: dlrs,
+    doRound: this.state.doRound,
+  });
+}
+
+adjustRounding=(event,isChecked) =>{
+  this.setState({
+    euros: this.state.euros,
+    dollars: this.state.dollars,
+    doRound: isChecked,
+  });
+}
+
+clear=(event,isChecked) =>{
+  this.setState({
+    euros: 0,
+    dollars: 0,
+    doRound: this.state.round,
+  });
+}
+
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <Paper style={{width: 600}}>
+          <div>
+          Euros: <TextField value={this.state.euros} onChange={(e, v)=> {this.updateEuros(v)}} id="euros"/>
+          </div> <div>
+          Dollars: <TextField value={this.state.dollars} onChange={this.updateDollars} id="dollars"/>
+          </div> <div>
+            <Checkbox label="Round" onCheck={this.adjustRounding}/>
+            </div> <div>
+              <RaisedButton label="Clear" onClick={this.clear}/>
+              </div>
+          </Paper>
+      </MuiThemeProvider>
     );
   }
 }
